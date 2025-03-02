b9b2ec26ce1e8b0cd42dc46d31ecde42cd7d3eb8
RJ Trenchard <rjtrenchard@gmail.com>
2025-03-01 19:59:32 -0800

    add map logic, more test data

--- forestry-club-hour-tracker/src/AdminReview.jsx
+++ forestry-club-hour-tracker/src/AdminReview.jsx
@@ -4,18 +4,24 @@ import ToastContainer from 'react-bootstrap/ToastContainer';
 
 const test_data = [
     {
+        ['work_id']: 0,
         ['name']: 'John Doe',
+        ['name_id']: 0,
         ['time_in']: Date.now(),
         ['time_out']: Date.now(),
         ['date_volunteered']: Date.now(),
-        ['date_submitted']: Date.now()
+        ['date_submitted']: Date.now(),
+        ['under_review']: true
     },
     {
+        ['work_id']: 1,
         ['name']: 'Bob Johnson',
+        ['name_id']: 1,
         ['time_in']: Date.now() + 1,
         ['time_out']: Date.now() + 1,
         ['date_volunteered']: Date.now(),
-        ['date_submitted']: Date.now() + 1
+        ['date_submitted']: Date.now() + 1,
+        ['under_review']: true
     }
 ]
 
@@ -24,15 +30,18 @@ function AdminReview() {
         <>
             <ToastContainer>
                 {
-                    test_data.map(o =>
-                        <AdminNotify
-                            key={o.name + o.date_submitted.toString()}
-                            name={o.name}
-                            time_in={o.time_in}
-                            time_out={o.time_out}
-                            date_submitted={o.date_submitted}
-                            date_volunteered={o.date_volunteered}
-                        />
+                    test_data.map(o => {
+                        o.under_review && (
+                            <AdminNotify
+                                key={o.name + o.date_submitted.toString()}
+                                name={o.name}
+                                time_in={o.time_in}
+                                time_out={o.time_out}
+                                date_submitted={o.date_submitted}
+                                date_volunteered={o.date_volunteered}
+                            />
+                        )
+                    }
                     )
                 }
             </ToastContainer>
